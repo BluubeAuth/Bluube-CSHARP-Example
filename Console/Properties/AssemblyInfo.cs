9cdf587ec8e47ec5bee6d1c4fa30e5de1003ea32
Bluube <bluubeshop@gmail.com>
2026-04-13 10:34:42 -0300

    Create AssemblyInfo.cs

---
+++ Console/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Bluube Auth Example")]
+[assembly: AssemblyDescription("Secure Licensing and Authentication Example for Bluube Auth")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bluube Services")]
+[assembly: AssemblyProduct("Bluube Licensing System")]
+[assembly: AssemblyCopyright("Copyright © Bluube 2026")]
+[assembly: AssemblyTrademark("Bluube")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("886ac4fb-026c-4f58-159e-3d4e8d807ff1")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
